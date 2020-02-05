9c279b4273fb67fbc51853ebfe33d6bb2fbb953d
Gamaranto <gamarantor@gmail.com>
2020-02-05 02:29:35 +0100

    Add layout to About Page

--- src/components/common/ProjectBox.js
+++ src/components/common/ProjectBox.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-export default function ProjectBox() {
-    return (
-        <div className="project-box">
-            <article className="project-content">
-                <h3>Notion.js</h3>
+export default function ProjectBox({ title, order }) {
+    return isOdd(order) ? (
+        <>
+            <article className="project-box">
+                <h3>{title}</h3>
             </article>
-        </div>
+            <div className="ws"></div>
+        </>
+    ) : (
+        <>
+            <div className="ws"></div>
+            <article className="project-box">
+                <h3>{title}</h3>
+            </article>
+        </>
     );
+}
+
+function isOdd(n) {
+    return n % 2 === 1;
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import { Layout, ProjectBox } from "../components/common";
 
+import "../styles/about.css";
+
 export default function About(props) {
     return (
         <Layout location={props.location}>
-            <section className=" projects">
-                <ProjectBox />
-                <ProjectBox />
-                <ProjectBox />
+            <section className="projects">
+                <ProjectBox title="1" order={1} />
+                <ProjectBox title="2" order={2} />
+                <ProjectBox title="3" order={3} />
+                <ProjectBox title="4" order={4} />
+                <ProjectBox title="5" order={5} />
             </section>
         </Layout>
     );
